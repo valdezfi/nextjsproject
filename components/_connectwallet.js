810025d3f3bd87160945f37d77dbab760ad8302b
Ral Andrew <randrew934@gmail.com>
2022-07-08 22:29:24 +0100

    08072023 UI Update

--- components/_connectwallet.js
+++ components/_connectwallet.js
@@ -4,35 +4,38 @@ import { injected } from "../components/wallet/connectors"
 import React, { useState } from 'react';
 
 
- function ConnectWalletButton() {
-    const { active, account, library, connector, activate, deactivate } = useWeb3React() 
+function ConnectWalletButton() {
+  const { active, account, library, connector, activate, deactivate } = useWeb3React();
+  //const [address, setAddress] = useState("");
 
 
-    async function connect() {
-        try {
-          await activate(injected)
-          localStorage.setItem('isWalletConnected', true)
-        } catch (ex) {
-          console.log(ex)
-        }
-      
-     async function disconnect() {
+  async function connect() {
     try {
-      deactivate()
-      localStorage.setItem('isWalletConnected', false)
+      await activate(injected)
+      localStorage.setItem('isWalletConnected', true)
     } catch (ex) {
       console.log(ex)
     }
   }
-  
-  }
+
+    const disconnect = async () => {
+      try {
+        deactivate()
+        localStorage.setItem('isWalletConnected', false)
+      } catch (ex) {
+        console.log(ex)
+      }
+    }
+
 
   useEffect(() => {
     const connectWalletOnPageLoad = async () => {
       if (localStorage?.getItem('isWalletConnected') === 'true') {
         try {
           await activate(injected)
+          alert(account)
           localStorage.setItem('isWalletConnected', true)
+          //  setAccountView(account.substring(0, 8) + '...');
         } catch (ex) {
           console.log(ex)
         }
@@ -42,13 +45,30 @@ import React, { useState } from 'react';
 
 
   return (
+    <div className="dropdown mx-sm-0 px-sm-0 mx-md-0 px-md-0 mx-lg-3 px-lg-5">
+      <button onClick={connect} className={active ? "hide" : "btn btn-light  btn-lg btn-block"}
+        type="button">Connect Wallet</button>
+      {active ?
+        <div className="dropdown mt-2 mx-sm-0 px-sm-0 mx-md-0 px-md-0 mx-lg-0 px-lg-0"><a className="dropdown-toggle mx-sm-0 p-sm-0" href="#" role="button"
+          id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" data-bs-target="#dropdownlink" 
+         aria-controls="dropdownlink">
+          <b>{
+            account.substring(0, 8) + '...' + account.charAt(account.length - 4) +
+            account.charAt(account.length - 3) + account.charAt(account.length - 2) +
+            account.charAt(account.length - 1)} </b>
+        </a>
 
-        <div className="row ps-5 pe-5">
-            <button onClick={connect} className="btn btn-light  btn-lg btn-block" type="button">Connect Wallet</button>
-            {active ? <span><b>{account} </b></span> : <span></span>}               
+          <ul class="dropdown-menu" id="dropdownlink" aria-labelledby="dropdownMenuLink">
+            <li><a class="dropdown-item" href="#" onClick={disconnect}>Logout</a></li>
+          </ul>
         </div>
 
-    );
+        :
+
+        <span></span>}
+    </div>
+
+  );
 }
 
 export default ConnectWalletButton;
--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,13 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+
       </Head>
       <body>
         <Main />
         <MegaFIScripts />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
       </body>
     </Html>
   )
