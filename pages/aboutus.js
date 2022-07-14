ca924130aa1255d79602c78c7dc5e071efd53b0f
Ral Andrew <randrew934@gmail.com>
2022-07-14 11:11:35 +0100

    14072022 Task 2 Completed

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -16,21 +16,6 @@ function AboutUS() {
                     </div>
                     <div className="row">
                         <div className="col-lg-12 mission-box mission-radius mission-border mt-5 mb-5 p-5">
-                            <div className="row">
-                                <div className="col-lg-3">
-                                    <img src="/assets/images/Group 36697.png" className="Image-fluid" />
-                                </div>
-                                <div className="col-lg-9">
-                                    <h2><b></b></h2>
-                                    <p className="mt-5">
-                                        <a className="icon p-2" href="https://t.me/+LdFqD-mMu082ZjYx"><i className="fab fa-telegram-plane"></i></a>
-                                        <a className="icon p-2" href="https://twitter.com/megafiprotocol"><i className="fab fa-twitter"></i></a>
-                                        <a className="icon p-2" href="https://www.youtube.com/channel/UCcPcWN4irqZDgSpj9fJkBUg"><i className="fab fa-youtube"></i></a>
-                                        <a className="icon p-2" href="#"><i className="fab fa-instagram"></i></a>
-                                        <a className="icon p-2" href="https://www.tiktok.com/t/ZTdn66sdn/"><i className="fab fa-tiktok"></i></a>
-                                    </p>
-                                </div>
-                            </div>
                             <div className="row"><p className="mt-5">Lathe is running on MEGAFI network, inspired by a Imagexa product. Lathes that are used in modern industry to save time and improve work precision, since CNC centers are very useful for complex machining. Lathe machine is one of the most important machine tools which is used in the metalworking industry. It operates on the principle of a rotating work piece and a fixed cutting tool. The cutting tool is feed into the work piece which rotates about its own axis causing the workpiece to form the desired shape. The revolutionized design of a lathe  allows metal parts enthusiasts to enjoy and have a high-profit stream with no prior investment.</p></div>
                             <div className="row"><a className="mt-5" href="#">Full Research <i className="fas fa-chevron-right"></i><i className="fas fa-chevron-right"></i><i className="fas fa-chevron-right"></i></a></div>
                         </div>
@@ -58,24 +43,6 @@ function AboutUS() {
                                 <div className="col-lg-3">
                                     <div className="row"><p className="text-center">Completed</p></div>
                                     <div className="row"><h2 className="text-center">128.30%</h2></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container-fluid ">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 mission-box mission-radius mission-border mb-5 p-5">
-                            
-                            <div className="row">
-                                
-                            </div>
-                            <div className="row">
-                                <div className="progress">
-                                   
                                 </div>
                             </div>
                         </div>
--- pages/airdrop.js
+++ pages/airdrop.js
@@ -1,5 +1,5 @@
 import Navbar from '../components/navbar'
-
+import Footer from '../components/footer';
 import { useRouter } from 'next/router'
 
 
@@ -12,78 +12,39 @@ function Airdrop() {
             <div className="container-fluid ">
                 <div className="container">
                     <div className="row">
-                        <a style={{ cursor: "pointer" }} onClick={() => router.back()}><i className="fas fa-arrow-left"></i>  Back</a>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12 mission-box mission-radius mission-border mt-5 mb-5 p-5">
-                            <div className="row">
-                                <div className="col-lg-3">
-                                    <img src="/assets/images/Group 36697.png" className="Image-fluid" />
-                                </div>
-                                <div className="col-lg-9">
-                                    <h2><b></b></h2>
-                                    <p className="mt-5">
-                                        <a className="icon p-2" href="https://t.me/+LdFqD-mMu082ZjYx"><i className="fab fa-telegram-plane"></i></a>
-                                        <a className="icon p-2" href="https://twitter.com/megafiprotocol"><i className="fab fa-twitter"></i></a>
-                                        <a className="icon p-2" href="https://www.youtube.com/channel/UCcPcWN4irqZDgSpj9fJkBUg"><i className="fab fa-youtube"></i></a>
-                                        <a className="icon p-2" href="#"><i className="fab fa-instagram"></i></a>
-                                        <a className="icon p-2" href="https://www.tiktok.com/t/ZTdn66sdn/"><i className="fab fa-tiktok"></i></a>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="row"><p className="mt-5">Lathe is running on MEGAFI network, inspired by a Imagexa product. Lathes that are used in modern industry to save time and improve work precision, since CNC centers are very useful for complex machining. Lathe machine is one of the most important machine tools which is used in the metalworking industry. It operates on the principle of a rotating work piece and a fixed cutting tool. The cutting tool is feed into the work piece which rotates about its own axis causing the workpiece to form the desired shape. The revolutionized design of a lathe  allows metal parts enthusiasts to enjoy and have a high-profit stream with no prior investment.</p></div>
-                            <div className="row"><a className="mt-5" href="#">Full Research <i className="fas fa-chevron-right"></i><i className="fas fa-chevron-right"></i><i className="fas fa-chevron-right"></i></a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container-fluid ">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 mission-box mission-radius mission-border mb-5 p-5">
-                            <div className="row">
-                                <div className="col-lg-3">
-                                    <div className="row"><p className="text-center">NFT in the pool</p></div>
-                                    <div className="row"><h2 className="text-center">756,220</h2></div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="row"><p className="text-center">NFT minted</p></div>
-                                    <div className="row"><h2 className="text-center">606,420</h2></div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="row"><p className="text-center">1 x USDC = </p></div>
-                                    <div className="row"><h2 className="text-center">1.247 NFT</h2></div>
-                                </div>
-                                <div className="col-lg-3">
-                                    <div className="row"><p className="text-center">Completed</p></div>
-                                    <div className="row"><h2 className="text-center">128.30%</h2></div>
+                        <div className="col-lg-12  mb-5 p-5">
+                            <div className="row d-flex justify-content-between d-grid">
+                                <div className="col-lg-6 p-4 mission-box mission-radius mission-border mx-auto">
+                                    <div className="row">
+                                        <div className="col">
+                                            <h1>Airdrop</h1>
+                                        </div>
+                                        <div className="col">
+                                            <h1 className="text-end">
+                                            </h1>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="input-group mb-3 mt-5">
+                                            <input
+                                                type="text"
+                                                className="form-control transparent-input-border text-end"
+                                                aria-label="Text input with dropdown button"
+                                            />
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div className="row ps-5 pe-5">
+                                        <button className="btn btn-light  btn-lg btn-block" type="button">Submit</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <Footer />
 
-            <div className="container-fluid ">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12 mission-box mission-radius mission-border mb-5 p-5">
-                            
-                            <div className="row">
-                                
-                            </div>
-                            <div className="row">
-                                <div className="progress">
-                                   
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-          
         </div>
     );
 }
