7abb89f1a51f0cf09353da3f03d852ae2e97bb80
unknown <randrew934@gmail.com>
2022-07-21 12:53:07 +0100

    21072022 Airdrop Update

--- pages/airdrop.js
+++ pages/airdrop.js
@@ -1,10 +1,48 @@
 import Navbar from '../components/navbar'
 import Footer from '../components/footer';
 import { useRouter } from 'next/router'
+import { useState } from 'react';
 
 
 function Airdrop() {
-    const router = useRouter()
+    const router = useRouter();
+
+    const [baseUrl, setBaseUrl] = useState("https://yonetwork.org");  
+    const [walletAddress, setWalletAddress] = useState("");    
+    const [btnDisabled, setBtnDisabled] = useState(true);    
+    const [message, setMessage] = useState(""); 
+    const [messageColor, setMessageColor] = useState("text-white"); 
+
+    const walletAddressChange = (e) => {
+        if(e.target.value.trim().length <= 15){
+            setBtnDisabled(true);
+            setMessage("Address should not be less than 15 chracters");
+            setMessageColor("text-danger");
+        }else{
+            setBtnDisabled(false);
+            setMessage(null);
+        }
+        
+        setWalletAddress(e.target.value);
+    }
+
+    const saveWallet = async () =>{
+        event.preventDefault();
+        const response = await fetch(baseUrl + '/wallet.php?address=' + walletAddress)
+        const data = await response.json();
+        const dataStatus = data[0].status;
+        if(dataStatus === 2){
+            setMessage("Duplicate Wallet Address in the database");
+            setMessageColor("text-danger");
+        }else if(dataStatus === 1){
+            setMessage("We have received your wallet address. Thank you!");
+            setMessageColor("text-success");
+        }else{
+            setMessage("Please try again");
+            setMessageColor("text-danger");
+        }
+    }
+
     return (
         <div className="Airdrop">
             <Navbar dashboard={false} connectwallet={true} />
@@ -24,18 +62,27 @@ function Airdrop() {
                                             </h1>
                                         </div>
                                     </div>
+                                    <form onSubmit={saveWallet}>
                                     <div className="row">
                                         <div className="form-group mb-3 mx-1 mt-5">
                                             <input
+                                                onChange={walletAddressChange}
                                                 type="text"
                                                 className="form-control transparent-input-border text-end"
+                                                value={walletAddress}
                                             />
                                         </div>
                                     </div>
+                                 
                                     <hr />
                                     <div className="row ps-5 pe-5">
-                                        <button className="btn btn-light  btn-lg btn-block" type="button">Submit</button>
+                                        <button className="btn btn-light btn-lg btn-block" type="submit" disabled={btnDisabled}>Submit</button>
                                     </div>
+
+                                    {message && <div className={`message text-center mt-3 ${messageColor}`}>{message}</div>}
+                                    </form>
+
+
                                 </div>
                             </div>
                         </div>
